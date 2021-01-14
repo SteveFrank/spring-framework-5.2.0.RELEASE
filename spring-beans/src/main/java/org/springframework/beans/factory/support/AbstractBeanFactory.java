04ae68b42ddcfe837194b21a7e1841869b3ac428
FrankQian <yangqian3@staff.weibo.com>
2021-01-14 19:54:28 +0800

    update

--- spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/AbstractBeanFactory.java
@@ -242,6 +242,8 @@ public abstract class AbstractBeanFactory extends FactoryBeanRegistrySupport imp
 	protected <T> T doGetBean(final String name, @Nullable final Class<T> requiredType,
 			@Nullable final Object[] args, boolean typeCheckOnly) throws BeansException {
 
+		// 通过三种方式获取beanName
+		// 一个是原始的beanName，一个是加了&的，一个是别名的
 		final String beanName = transformedBeanName(name);
 		Object bean;
 
---
+++ spring-demo/src/main/java/com/demo/dao/impl/BoyFriend.java
@@ -0,0 +1,14 @@
+package com.demo.dao.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author yangqian
+ * @date 2021/1/14
+ */
+public class BoyFriend {
+
+	@Autowired
+	private GirlFriend girlFriend;
+
+}
